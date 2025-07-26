29bc73bea9cc6906774b5793a81ef29bf99472b9
kazkt01 <diminishdanish@gmail.com>
2025-07-27 01:29:05 +0900

    save

---
+++ memo.tsx
@@ -0,0 +1,15 @@
+
+
+// <Card>
+// <CardHeader>
+//   <CardTitle>Card Title</CardTitle>
+//   <CardDescription>Card Description</CardDescription>
+//   <CardAction>Card Action</CardAction>
+// </CardHeader>
+// <CardContent>
+//   <p>Card Content</p>
+// </CardContent>
+// <CardFooter>
+//   <p>Card Footer</p>
+// </CardFooter>
+// </Card>
