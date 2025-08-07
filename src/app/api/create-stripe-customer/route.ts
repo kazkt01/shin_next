8be4f454d47c41ed3102654d0aa419de8250c5b0
kazkt01 <diminishdanish@gmail.com>
2025-08-07 22:21:03 +0900

    save

--- src/app/api/create-stripe-customer/route.ts
+++ src/app/api/create-stripe-customer/route.ts
@@ -1,20 +1,35 @@
 // サブスク決済のポストリクエストを送った時のストライプへの顧客データ保存API
 // npm i stripe でストライプのモジュールをインストール
 
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { NextRequest, NextResponse } from "next/server";
 import initiStripe  from "stripe"; 
+import {cookies} from "next/headers";
 
 
 export async function POST(req: NextRequest) {
+    // ここで初期化
+    const supabase = createRouteHandlerClient({cookies})
+
     
     const date = await req.json();
-    const {email} = date;
+    const {id, email} = date;
     
     const stripe = new initiStripe(process.env.STRIPE_SECRET_KEY!);
     
     const customer = await stripe.customers.create({
         email,
+    });
+
+    const { error } =  await supabase
+    .from("profile")
+    .update({
+        stripe_customer: customer.id,
     })
+    .eq("id", id);
+
+    console.log(error);
+
 
     return NextResponse.json({
         message: `stripe customer created: ${customer.id}`,
